703eddc1703d1503ec823a70866a7bcd07fe2f33
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-02 17:39:40 +0100

    Esercizio concluso

--- Palindromo/js/main.js
+++ Palindromo/js/main.js
@@ -1,25 +1,34 @@
 let parolaUtente = prompt ('inserisci una parola')
 
 let risultato = numeroPalindromo (parolaUtente);
-console.log(risultato)
-
+console.log (risultato)
 
 function numeroPalindromo (parolaUtente) {
 
-    let palindromo;
-    let i = parolaUtente[0];
-    let d = parolaUtente.length - 1;
+    let palindrome = false;
+    let i = 0;
+    let d = parolaUtente.length -1 ;
 
-    while (i <= parolaUtente.length - 1) {
-     
-        if (i == d) {
+    while (i <= parolaUtente.length) {
+
+        if (i > d) {
+            palindrome = true;
+            i = parolaUtente.length + 1;
+
+        } else if ( i < parolaUtente.length && parolaUtente[i] == parolaUtente[d]) {
             i++;
             d--;
-            console.log ('palindromo')
         } else {
-            console.log = 'non è palindromno';
-        };
-        i++;
+            i = parolaUtente.length + 1
+        }
     }
+
+        if (palindrome == true) {
+            console.log ('palindroma')
+        } else {
+            console.log ('non palindroma')
+        }
+        return palindrome;
+
 
 }

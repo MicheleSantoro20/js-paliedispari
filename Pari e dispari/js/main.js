e39036140ada01f1051a5e73100158401985eea1
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-02 14:35:39 +0100

    Esercizio riuscito, tentativo di risoluzione relativo al console.log undefined

---
+++ Pari e dispari/js/main.js
@@ -0,0 +1,22 @@
+let numeroUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
+let computer = numerocasuale (); 
+
+console.log (numeroUser)
+console.log  (numerocasuale());
+
+function numerocasuale () {
+    const random = Math.floor (Math.random() * (5 - 1 + 1)) + 1;
+    return random;
+};
+
+
+//Condizione di vittoria = Pari vince l'utente, dispari vince il computer
+
+let somma = numeroUser + computer;
+console.log ('somma = ' + somma)
+
+if (somma % 2 == 0) {
+    console.log ('il numero è ' + somma + ' il vincitore sei tu!');
+} else {
+    console.log ('il numero è ' + somma + ' il vincitore è il computer')
+};

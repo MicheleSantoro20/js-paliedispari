054735ea130ebb25e32b76d08b9766a9928b6cca
Michele Santoro <michelesantoro2000@icloud.com>
2023-03-02 14:36:16 +0100

    Pari dispari concluso. Tentativo soluzione console.log Undefined

--- Pari e dispari/js/main.js
+++ Pari e dispari/js/main.js
@@ -2,10 +2,10 @@ let numeroUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
 let computer = numerocasuale (); 
 
 console.log (numeroUser)
-console.log  (numerocasuale());
+console.log  (computer);
 
 function numerocasuale () {
-    const random = Math.floor (Math.random() * (5 - 1 + 1)) + 1;
+    const random = Math.floor (Math.random() * (5)) + 1;
     return random;
 };
 
@@ -15,8 +15,15 @@ function numerocasuale () {
 let somma = numeroUser + computer;
 console.log ('somma = ' + somma)
 
-if (somma % 2 == 0) {
-    console.log ('il numero è ' + somma + ' il vincitore sei tu!');
-} else {
-    console.log ('il numero è ' + somma + ' il vincitore è il computer')
+
+function vincitore (somma) {
+
+    if (somma % 2 == 0) {
+        console.log ('il numero è ' + somma + ' il vincitore sei tu!');
+    } else {
+        console.log ('il numero è ' + somma + ' il vincitore è il computer')
+    };
+
 };
+
+console.log (vincitore(somma));
